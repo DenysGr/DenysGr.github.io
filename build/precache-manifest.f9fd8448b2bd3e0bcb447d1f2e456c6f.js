41a4ace9bc113f5257e461d97d4f12deb2323c94
DenysGr <45840544+DenysGr@users.noreply.github.com>
2019-03-25 00:11:20 +0800

    seeing how it works

---
+++ build/precache-manifest.f9fd8448b2bd3e0bcb447d1f2e456c6f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "22e05b86a5bc56cf75b9ab7683fa4309",
+    "url": "/static/media/Alien Robot.22e05b86.woff"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e087e7dad988229b1d9f",
+    "url": "/static/js/main.b79ae129.chunk.js"
+  },
+  {
+    "revision": "8634eeb56562b3e5cead",
+    "url": "/static/js/2.7cb68033.chunk.js"
+  },
+  {
+    "revision": "e087e7dad988229b1d9f",
+    "url": "/static/css/main.bbe7f965.chunk.css"
+  },
+  {
+    "revision": "8634eeb56562b3e5cead",
+    "url": "/static/css/2.e9ab1e23.chunk.css"
+  },
+  {
+    "revision": "62ee55d5abae181998e308fb909da611",
+    "url": "/index.html"
+  }
+];
